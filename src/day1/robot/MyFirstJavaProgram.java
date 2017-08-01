6b6f892472115df26919421edb0f9ec8b2703232
ThomasTwomey <league@WTS15.attlocal.net>
2017-08-01 11:52:55 -0700

    robot from day 2

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,12 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot r2d2 = new Robot();
+r2d2.unSparkle();
+r2d2.move(200);
+r2d2.setSpeed(5);
+r2d2.penDown();
+r2d2.move(100);
+r2d2.turn(90);
 
 	
-		
-		
-	}
-}
